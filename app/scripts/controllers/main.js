f043f13cd42eb2d0a5906cd1ffa46a5bb77e1c56
Robur <anton.ulianov@gmail.com>
2013-07-25 22:58:25 +0300

    Initial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('piApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
